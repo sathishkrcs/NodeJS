8c068c8a566876b1efc707610dbc0be15a453897
Sathish Kumar P <sathishkr.cs@gmail.com>
2016-07-15 17:55:25 +0530

    Create angular-code.js
    First Commit

---
+++ angular-code.js
@@ -0,0 +1,9 @@
+
+    // Value Can be Overidden 
+    ezWeb.value("Company", "CTS");
+    ezWeb.value("Company", "CTS United States of America");
+    
+    // Constant cannot be Overidden
+    ezWeb.constant("Name", "Sathish");
+    ezWeb.constant("Name", "Sathish Da");
+  
