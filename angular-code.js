f902bd5283e8d230cd55c0404551b7676ee7d0f5
Sathish Kumar P <sathishkr.cs@gmail.com>
2016-07-16 14:20:17 +0530

    Delete angular-code.js

--- angular-code.js
+++
@@ -1,9 +0,0 @@
-
-    // Value Can be Overidden 
-    ezWeb.value("Company", "CTS");
-    ezWeb.value("Company", "CTS United States of America");
-    
-    // Constant cannot be Overidden
-    ezWeb.constant("Name", "Sathish");
-    ezWeb.constant("Name", "Sathish Da");
-  
